e746beb73fa5708062da9314eb1ae447a75398a3
skarbovskiy <george.skarbovskiy@gmail.com>
2016-03-09 14:40:50 +0200

    added cache purge

--- actions/index.js
+++ actions/index.js
@@ -1,7 +1,3 @@
-/**
- * Created by justlogin on 10/21/14.
- */
-
 exports.authorize = require('./authorize');
 exports.uploadFile = require('./upload-file');
 exports.uploadFileByContent = require('./upload-file-by-content');
@@ -9,3 +5,4 @@ exports.createLink = require('./create-link');
 exports.deleteFile = require('./delete-file');
 exports.getContainerFiles = require('./get-container-files');
 exports.createContainer = require('./create-container');
+exports.clearCache = require('./clear-cache');
